3f27872055a300381541679fb3df564c20e6abb7
margret <margretmicheal93@gmail.com>
2026-03-07 17:58:07 +0200

    Refactor GetAllSkillsGroupedQueryHandler

--- Mosahem.Application/Interfaces/Repositories/ISkillRepository.cs
+++ Mosahem.Application/Interfaces/Repositories/ISkillRepository.cs
@@ -1,4 +1,5 @@
 ﻿using mosahem.Domain.Entities.MasterData;
+using Mosahem.Application.Features.Skills.Queries.GetAllSkillsGrouped;
 
 namespace mosahem.Application.Interfaces.Repositories
 {
@@ -9,5 +10,6 @@ namespace mosahem.Application.Interfaces.Repositories
         Task<bool> AreAllExistingAsync(IReadOnlyCollection<Guid> skillIds, CancellationToken cancellationToken = default);
         Task<IReadOnlyList<Skill>> GetAllWithFieldAsync(CancellationToken cancellationToken = default);
         Task<IReadOnlyList<Skill>> GetAllForListingAsync(CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<GetAllSkillsGroupedQueryResponse>> GetAllGroupedAsync(IReadOnlyList<Guid>? fieldIds, CancellationToken cancellationToken = default);
     }
 }
