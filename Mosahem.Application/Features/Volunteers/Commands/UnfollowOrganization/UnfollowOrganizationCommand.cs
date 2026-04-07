2b9ad982f9be905e0015fc1ae6866f083debbb50
margret <margretmicheal93@gmail.com>
2026-04-07 23:30:44 +0200

    (feat) unfollow organization feature for volunteers
    Implemented UnfollowOrganizationCommand, handler, and validator to allow volunteers to unfollow organizations. Added a new HTTP DELETE endpoint in VolunteersController and updated routing. Validation ensures both volunteer and organization exist and that the follow relationship is present before removal.

---
+++ Mosahem.Application/Features/Volunteers/Commands/UnfollowOrganization/UnfollowOrganizationCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using mosahem.Application.Common;
+
+namespace Mosahem.Application.Features.Volunteers.Commands.UnfollowOrganization
+{
+    public class UnfollowOrganizationCommand : IRequest<Response<string>>
+    {
+        public Guid VolunteerId { get; set; }
+        public Guid OrganizationId { get; set; }
+    }
+}
